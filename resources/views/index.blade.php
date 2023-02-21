bb8698b4fc9319fa8dce6fdca6172d5ba94b1d87
Pierre YEBAS <pierre.yebas@gmail.com>
2023-02-21 20:43:38 +0100

    Test1 avec caleb

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href={{ asset("./vendors/bootstrap/css/bootstrap.min.css") }}>
-    <link rel="stylesheet" href={{ asset("./vendors/font-awesome/css/all.min.css") }}>
-    <link rel="stylesheet" href={{ asset("./css/style.css") }}>
-    <link rel="shortcut icon" href={{ "./img/fav-icon.png"  }} type="image/x-icon">
+    <link rel="stylesheet" href={{ asset("vendors/bootstrap/css/bootstrap.min.css") }}>
+    <link rel="stylesheet" href={{ asset("vendors/font-awesome/css/all.min.css") }}>
+    <link rel="stylesheet" href={{ asset("css/style.css") }}>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href={{ asset("img/fav-icon.png")  }} type="image/x-icon">
     <title>ZOE | Transport et logistique</title>
 </head>
 
